f8b3c092d1be909c0c97e140228c9d04af2c8760
Keval Dhandhukiya <kevaldhandhukiya23@gmail.com>
2026-07-21 16:48:13 +0530

    Add files via upload

---
+++ ASP.NET core/Extra_1/Extra_1/Extra_7.cs
@@ -0,0 +1,12 @@
+﻿//Person p12 = new Person();
+//p12.displayDetails();
+//Person p1 = new EmpDemo();
+//p1.displayDetails();
+//Person p2 = new Manager();
+//p2.displayDetails();
+
+
+
+//Emp e1 = new FullTimeEmp();
+//e1.calculateSalary();
+//e1.displayEmpDetails();
