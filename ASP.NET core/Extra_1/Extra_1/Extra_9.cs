037e59a98006e1cbd1b18f5283746bb2c36d75e0
Keval Dhandhukiya <kevaldhandhukiya23@gmail.com>
2026-07-21 16:40:22 +0530

    Add files via upload

---
+++ ASP.NET core/Extra_1/Extra_1/Extra_9.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra_1
+{
+    internal class Calculator
+    {
+        public void multiply(int a, int b)
+        {
+            int multiply = a * b;
+            Console.WriteLine(multiply);
+        }
+
+        public void multiply(double a, double b)
+        {
+
+            double multiply = a * b;
+            Console.WriteLine(multiply);
+        }
+
+        public void multiply(float a, float b)
+        {
+
+            float multiply = a * b;
+            Console.WriteLine(multiply);
+        }
+    }
+}
